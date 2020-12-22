33973add2f8df386e309feec20bcb3109a0da64e
Robin Hahne <robin.hahne@yooapps.com>
2020-12-22 10:43:46 +0100

    remove pollyfill

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: '@yoo-digital/eslint-config-react',
   rules: {
     'react/jsx-props-no-spreading': [1],
-    'max-lines-per-function': ['warning', { 'max': 100 }],
+    'max-lines-per-function': [1, { 'max': 100 }],
   },
 };
